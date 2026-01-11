b9f9768dcc1ca6b14d109b52e2d394d4be9b6abf
Mihai Hiticas <hiticasmihai7@gmail.com>
2026-01-11 13:16:59 +0200

    fix typo

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
       </div>
       <div className="hero-content">
         <Typography as="h1" type="heading-xl" className="hero-title">Nice to <br className="break"/> meet you! <br className="break-2"/>I’m <span className='green-underline'>Mihai Hiticaș.</span></Typography>
-        <Typography type="body" className="hero-subtitle">Based in the Timișoara, Romania. I’m a front-end developer passionate about building accessible web apps that users love.</Typography>
+        <Typography type="body" className="hero-subtitle">Based in Timișoara, Romania. I’m a front-end developer passionate about building accessible web apps that users love.</Typography>
         <a href="mailto:hiticasmihai7@gmail.com">
           <Button aria-label="Contact">
             <Typography as="span" type="body-btn">Contact me</Typography>
